94e3220e5392702c345c6c8049098231867d045d
tnadeem <tayyab-nadeem@bayt.net>
2022-04-29 19:08:05 +0500

    crud operations and routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -4,7 +4,7 @@ let Exercise = require('../models/exercise.model');
 router.route('/').get((req, res) => {
     Exercise.find()
         .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json('Erroe: ' + err));
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -13,7 +13,7 @@ router.route('/add').post((req, res) => {
     const duration = req.body.duration;
     const date = req.body.date;
 
-    const newExercise = new User({
+    const newExercise = new Exercise({
         username,
         description,
         duration,
@@ -21,7 +21,34 @@ router.route('/add').post((req, res) => {
     });
 
     newExercise.save()
-        .then(() => res.json('User Added'))
+        .then(() => res.json('Exercise Added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Exercise Deleted!"))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json("Exercise updated!"))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
+    console.log("list users");
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
